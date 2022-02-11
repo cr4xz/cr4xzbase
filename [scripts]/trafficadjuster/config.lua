1291cacafed0300f80bb23f470f69878db085e9b
cr4xz <99499281+cr4xz@users.noreply.github.com>
2022-02-11 22:58:06 +0500

    Add files via upload

---
+++ [scripts]/trafficadjuster/config.lua
@@ -0,0 +1,16 @@
+TrafficAmount = 0.0
+PedestrianAmount = 0
+ParkedAmount = 0
+EnableDispatch = false
+
+
+--[[
+	TrafficAmount changes how much traffic there is on the Roads, this goes from 100% to 0%, values over 100% are also supported but not recommended, under 0% will cause a game crash.
+	PedestrianAmount changes how many Peds roam the Streets
+	ParkedAmount changes how many Parked Cars there are in the world
+	EnableDispatch is a toggle between true and false, this enables/disables various features such as:
+		
+
+
+]]
+
